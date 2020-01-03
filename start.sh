c22a7f325b766cddc2397eb561a172a3e98830b0
Arnaud Guillotin <arnaud.guillotin@epita.fr>
2020-01-03 14:38:55 +0100

    Added local-web-server in start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum -y install git npm
 git clone https://github.com/arnaudguillotin/AWS-PROJECT-Front.git
 cd AWS-PROJECT-Front
-npm install webpack webpack-cli dotenv-webpack
+npm install webpack webpack-cli dotenv-webpack local-web-server
 ./node_modules/webpack/bin/webpack.js --env.ENVIRONMENT=production --config webpack.config.js
+ws
