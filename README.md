35d74ae1d6b65f36b555413d17f7bf0cf5de42a1
Arnaud GUILLOTIN <arnaud.guillotin@epita.fr>
2020-01-03 15:12:56 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AWS-PROJECT---Front
+
+## Install commands:
+
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo yum -y install git npm
+git clone https://github.com/arnaudguillotin/AWS-PROJECT-Front.git
+cd AWS-PROJECT-Front
+
+## Start command:
+sh start.sh
--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-sudo yum -y install git npm
-git clone https://github.com/arnaudguillotin/AWS-PROJECT-Front.git
-cd AWS-PROJECT-Front
 npm install webpack webpack-cli dotenv-webpack local-web-server
 ./node_modules/webpack/bin/webpack.js --env.ENVIRONMENT=production --config webpack.config.js
 ./node_modules/local-web-server/bin/cli.js
